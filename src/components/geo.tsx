c9970b952081faefd7211e0bf3745fba0cd3aacc
GustavoLima67 <lgusta333@gmail.com>
2024-02-29 16:04:51 -0300

    Adding folder componens & Square, Circle

---
+++ src/components/geo.tsx
@@ -0,0 +1,12 @@
+export const Square = () => {
+    return (
+        <div className="w-52 h-52 bg-orange-700 text-white">texto qualquer inspirador</div>
+    )
+}
+
+export const Circle = () => {
+    return (
+        <div className="w-52 h-52 bg-green-700 text-white rounded-full">texto qualquer inspirador</div>
+    )
+}
+
