04ffe35aac219db9cce5655fc41ca00eacd16875
GustavoLima67 <lgusta333@gmail.com>
2024-03-01 14:08:20 -0300

    Aprendendo novas funcionalidades no ReactJS next & aprendendo a usar data

--- src/components/person.tsx
+++ src/components/person.tsx
@@ -1,10 +1,25 @@
+const getWeekday = (today: Date) => {
+    return new Intl.DateTimeFormat('pt-br',  {weekday: 'long'}).format(today);
+}
+
 export const Person = () => {
+    const data = {
+        name: 'Elon Musk',
+        avatar: 'https://th.bing.com/th/id/OIP.O4Lrw3QH_SZeKwid1-Ha7QAAAA?rs=1&pid=ImgDetMain',
+        roles: ['Ceo da Tesla', 'Ceo da SpaceX']
+    }
+
+    const today: Date = new Date();
+
     return (
         <>
-            <h1>Elon Musk</h1>
-            <img src="https://th.bing.com/th/id/OIP.O4Lrw3QH_SZeKwid1-Ha7QAAAA?rs=1&pid=ImgDetMain" alt="Elon Musk" className="w-40"/>
+            <h1 style={{color: 'red', fontSize: '30px'}}>{data.name} - {getWeekday(today)}</h1>
+            <img src={data.avatar} alt={data.avatar} className="w-40"/>
 
-            <ul><li>Ceo da TESLA</li> <li>Ceo da SPACEX</li> <li>Ceo da...</li></ul>
+            <ul>
+                <li>{data.roles[0]}</li>
+                 <li>{data.roles[1]}</li>
+            </ul>
         </>
     )
 }
