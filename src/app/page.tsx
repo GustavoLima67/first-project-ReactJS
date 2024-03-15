0d5e73955cc57a08ba6f5c417e3874c4a0dd6c28
GustavoLima67 <lgusta333@gmail.com>
2024-03-15 14:58:48 -0300

    Adicionando Children no Componente

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,18 @@
-import { Person } from "@/components/person";
+import { Card } from "@/components/card";
 
 const Page = () => {
   return (
     <div>
-      <Person 
-        name='Elon Musk'
-        avatar='https://th.bing.com/th/id/OIP.O4Lrw3QH_SZeKwid1-Ha7QAAAA?rs=1&pid=ImgDetMain'
-        roles={['Ceo da Amazon', 'Ceo da Blue Origin']}
-      />
-      <Person 
-        name='Elon Musk'
-        avatar='https://th.bing.com/th/id/R.7f74cdafb3416c87fcf605e66f4c12fd?rik=20zeF75LZuhQ7Q&riu=http%3a%2f%2fwww.thefamouspeople.com%2fprofiles%2fimages%2felon-musk-1.jpg&ehk=bGWEbcsdoxqEEwaNuveQSXmdQ1%2b%2bOFWRbnwdiGtgLoM%3d&risl=&pid=ImgRaw&r=0'
-        roles={['Ceo da SpaceX', 'Ceo da Tesla']}
-      />
+      <h1 className="font-bold text-2xl">Ola mundo</h1>
+      <h3>Algum 3utro texto</h3> 
 
-      <Person 
-      name='João'
-      roles={['Algo aleatorio', 'Outra coisa']}
-      />
-      
-    </div>
+      <Card>
+        <>
+          <h3 className="text-3xl font-bold italic">Frase de Efeito</h3>
+          <p className="text-right text-sm">autor desconhecido</p>
+        </>
+      </Card>
+    </div>  
   )
 }
 
