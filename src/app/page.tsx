8be8b9208e711e667aca1224c9234208481d0b81
Lucas Lima <lucas.souza3146@gmail.com>
2024-03-19 09:18:19 -0300

    Rederização Condcional

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,10 @@ const Page = () => {
   return (
     <div>
       <h1 className="font-bold text-2xl">Ola mundo</h1>
-      <h3>Algum 3utro texto</h3> 
+      <h3>Algum outro texto</h3> 
 
-      <Card>
-        <>
-          <h3 className="text-3xl font-bold italic">Frase de Efeito</h3>
-          <p className="text-right text-sm">autor desconhecido</p>
-        </>
-      </Card>
+      
+      <Card phrase="Alguma frase top" author="Algum cara"/>
     </div>  
   )
 }
