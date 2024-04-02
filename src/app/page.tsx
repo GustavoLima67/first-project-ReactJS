d2175cd6924f1696d9127e381ddb2a2770fcd51d
GustavoLima67 <lgusta333@gmail.com>
2024-04-02 14:47:21 -0300

    adicioanando Array

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,20 @@
 import { Card } from "@/components/card";
-
 const Page = () => {
+
+  const People = [
+    {id: 1, name: "Fulano", prefission: "mathematician"},
+    {id: 2, name: "Ciclano", prefission: "chemist"},
+    {id: 3, name: "Beltrano", prefission: "phcisist"},
+    {id: 4, name: "Glutano", prefission: "chemist"},
+    {id: 5, name: "Jungulano", prefission: "astrophysicist"},
+  ]
+
+  
   return (
     <div>
       <h1 className="font-bold text-2xl">Ola mundo</h1>
       <h3>Algum outro texto</h3> 
 
-      
-      <Card phrase="Alguma frase top" author="Algum cara"/>
     </div>  
   )
 }
