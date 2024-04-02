92a379beb6d9a1b8243fc79e23e0d0389efb7257
GustavoLima67 <lgusta333@gmail.com>
2024-04-02 15:29:06 -0300

    Renderizando lista

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,19 @@
-import { Card } from "@/components/card";
+import {peopleList} from '@/data/people';
+
 const Page = () => {
 
-  const People = [
-    {id: 1, name: "Fulano", prefission: "mathematician"},
-    {id: 2, name: "Ciclano", prefission: "chemist"},
-    {id: 3, name: "Beltrano", prefission: "phcisist"},
-    {id: 4, name: "Glutano", prefission: "chemist"},
-    {id: 5, name: "Jungulano", prefission: "astrophysicist"},
-  ]
-
-  
   return (
+
     <div>
       <h1 className="font-bold text-2xl">Ola mundo</h1>
       <h3>Algum outro texto</h3> 
+
+      {peopleList.length > 0 &&
+        <ul>
+          {peopleList.map(person => 
+          <li key={person.id}> {person.name} - {person.profession}</li>)}
+        </ul>
+      }
 
     </div>  
   )
