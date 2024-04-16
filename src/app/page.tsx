6fbee1c827c9ca2513ab4a793f13cb137b8d08a1
Lucas Lima <lucas.souza3146@gmail.com>
2024-04-16 12:35:19 -0300

    Exercicio com Greeting parte 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,28 @@
-import {peopleList} from '@/data/people';
 
 const Page = () => {
 
-  const chemists = peopleList.filter(person => person.profession === 'chemist');
+  const fullTime = new Intl.DateTimeFormat('pt-BR', {
+    timeStyle: 'short',
+    hour12: false
+  }).format();
+
+  const hour = new Date().getHours();
+  let greeting = '';
+
+  if (hour >= 0 && hour < 12) {
+    greeting = 'Bom dia';
+  } else if (hour >= 12 && hour < 18) {
+    greeting = 'Boa tarde'
+  } else if (hour >= 18 && hour <= 23) {
+    greeting = 'Boa noite'
+  }
 
   return (
+    <div className="w-screen h-screen flex flex-col justify-center items-center text-white bg-gradient-to-r from-sky-500 to-indigo-500">
+      <div className="text-9xl 000">{fullTime}</div>
+      <div className="text-5xl font-bold">{greeting}</div>
 
-    <div>
-      <h1 className="font-bold text-2xl">Ola mundo</h1>
-      <h3>Algum outro texto</h3> 
-
-      {chemists.length > 0 &&
-        <>
-          <h3>Lista de Quimicos</h3>
-          <ul>
-            {chemists.map(person => 
-            <li key={person.id}>{person.name}</li> )}
-          </ul>
-        </>
-      }
-
-    </div>  
+    </div>
   )
 }
 
