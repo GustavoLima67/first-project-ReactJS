d4eee1486baa5492cb38545d1819a082655b4786
GustavoLima67 <lgusta333@gmail.com>
2024-06-05 15:13:36 -0300

    Rating via emoji (facil)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,16 @@
+import EmojiRating from "@/components/emojiRating";
 
 const Page = () => {
 
-  const fullTime = new Intl.DateTimeFormat('pt-BR', {
-    timeStyle: 'short',
-    hour12: false
-  }).format();
-
-  const hour = new Date().getHours();
-
-  return (
-    <div className="w-screen h-screen flex flex-col justify-center items-center text-white bg-gradient-to-r from-sky-500 to-indigo-500">
-      <div className="text-9xl 000">{fullTime}</div>
-      <div className="text-5xl font-bold">
-        {hour >=  0 && hour < 12 && "Bom dia"} 
-        {hour >= 12 && hour < 18 && "Boa tarde"}
-        {hour >= 18 && hour < 23 && "Boa noite"}
-      </div>
-
+  return(
+  
+    <div className="w-screen h-screen flex justify-center items-center">
+      <EmojiRating rate={4.5}/>
     </div>
+
   )
-  // obs: caso as duas condicionais estejam corretas, pode acarretar um erro nas gigantesco
+
+
 }
 
 export default Page;
