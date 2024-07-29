830e80865fb2a1919940afc7cecb5c1f8cb7411c
Lucas Lima <lucas.souza3146@gmail.com>
2024-07-29 15:14:00 -0300

    Using preventDefault in ReactNext

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,21 @@
 "use client"
-import { CustomButton } from "@/components/customButton";
+
+import { FormEvent } from "react";
 
 const Page = () => {
 
-  const handleButtonClick1 = () => alert('Clicou no botão 1');
-  const handleButtonClick2 = () => alert('Clicou no botão 2');
-  const handleButtonClick3 = () => alert('Clicou no botão 3');
+  const handleFormSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    alert("Opa, mandando...")
+  }
 
   return(
-    <div className="w-screen h-screen flex justify-center items-center">
-      <CustomButton label="Clique aqui" onClick={handleButtonClick1}/>
-      <CustomButton label="Clique aqui 2" onClick={handleButtonClick2}/>
-      <CustomButton label="Clique aqui 3" onClick={handleButtonClick3}/>
+    <div className="w-screen h-screen flex flex-col justify-center items-center">
+      <h1 className="text-5xl mb-3">Form de Login</h1>
+      <form onSubmit={handleFormSubmit}>
+        <input type="text" />
+        <input type="submit" value="Enviar" />
+      </form>
     </div>
   )
 
