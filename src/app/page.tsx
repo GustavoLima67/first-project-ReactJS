1d66801aee8e2b8ccdc2e74be676da7089402d1c
GustavoLima67 <lgusta333@gmail.com>
2024-08-30 15:13:51 -0300

    Usando States em campos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,20 +3,20 @@
 import { useState } from "react";
 
 const Page = () => {
+  const [nameInput, setNameInput] = useState('');
+
+  const handleBtnClick = () => alert("seu nome é "+ nameInput);
   
-  const [showSecret, setShowSecret] = useState(false); 
-
-  const handleClickButton = () => {
-    setShowSecret(!showSecret);
-  }
-
   return(
     <div className="w-screen h-screen flex flex-col justify-center items-center text-3xl">
-      <button onClick={handleClickButton} className="bg-blue-500 p-3">{showSecret ? 'Ocultar' : 'Mostrar'}</button>
-
-      {showSecret &&
-      <div className="p-3 bg-blue-300 rounded-md mt-3">Area secreta</div>
-      }
+     <input type="text" 
+     className="border border-black p-3 text-3xl text-black rounded"
+     placeholder="Digite seu nome"
+     value={nameInput}
+     onChange={e => setNameInput(e.target.value)}/>
+     <p>seu nome é {nameInput} </p>
+    <button onClick={handleBtnClick}>Mostrar valor do campo</button>
+    
     </div>
   );
 
